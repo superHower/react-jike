7ad837d8d38ad1ed1c2750ff39f7275ed0a43d58
super_hower <yhhwwgg@gmail.com>
2024-02-21 19:39:40 +0800

    3. 加入 登陆校验逻辑

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -10,10 +10,29 @@ const Login = () => {
         <img className="login-logo" src={logo} alt="" />
         {/* 登录表单 */}
         <Form>
-          <Form.Item>
+        <Form.Item
+            name="mobile"
+            // 多条校验逻辑 先校验第一条 第一条通过之后再校验第二条
+            rules={[
+              {
+                required: true,
+                message: '请输入手机号',
+              },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: '请输入正确的手机号格式'
+              }
+            ]}>
             <Input size="large" placeholder="请输入手机号" />
           </Form.Item>
-          <Form.Item>
+          <Form.Item
+            name="code"
+            rules={[
+              {
+                required: true,
+                message: '请输入验证码',
+              },
+            ]}>
             <Input size="large" placeholder="请输入验证码" />
           </Form.Item>
           <Form.Item>
