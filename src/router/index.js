8eef7fe4e6a6ee0022c796a9cef0ef2e634a2de8
super_hower <yhhwwgg@gmail.com>
2024-02-22 17:48:28 +0800

    2-8: feat: 封装AuthRoute组件，实现无token，强制跳转登陆页

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,12 @@ import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
 
 import { createBrowserRouter } from 'react-router-dom'
+import { AuthRoute } from '@/components/AuthRoute'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />
+    element: <AuthRoute><Layout /></AuthRoute> // 通过AuthRoute包裹Layout组件，实现登录验证
   },
   {
     path: "/login",
