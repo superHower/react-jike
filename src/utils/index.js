4efccaa530adcbd80bd16daab2f65f3d909afac6
super_hower <yhhwwgg@gmail.com>
2024-02-22 17:27:13 +0800

    2-6: 开发： 封装 token的增改删

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,13 @@
 // 统一中转工具模块函数
-import { request }  from "./request"
+// import {request} from '@/utils'
+
+import { request } from './request'
+
+import { setToken, getToken, removeToken } from './token'
 
 export {
-  request
+  request,
+  setToken,
+  getToken,
+  removeToken
 }
