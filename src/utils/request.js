f593a2107ebb22baa58218ef6bfe093cdf155daf
super_hower <yhhwwgg@gmail.com>
2024-02-23 15:45:24 +0800

    3-3: feat 处理Token失效

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 // axios的封装处理
 import axios from "axios"
-import { getToken, } from "./token"
+import { getToken, removeToken } from "./token"
+import router from "../router"
 
 const request = axios.create({
   baseURL: 'http://geek.itheima.net/v1_0',
@@ -24,7 +25,12 @@ request.interceptors.response.use((response) => {
 
   return response.data
 }, (error) => {
-
+  console.dir(error)
+  if (error.response.status === 401) {
+    removeToken()
+    router.navigate('/login')
+    window.location.reload()
+  }
   return Promise.reject(error)
 })
 
