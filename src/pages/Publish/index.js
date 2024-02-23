c2ce1548011775bd17ede21febd3058449d1a9de
super_hower <yhhwwgg@gmail.com>
2024-02-23 17:31:14 +0800

    3-11: feat 频道数据获取useEffect + 渲染list.map

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -15,10 +15,22 @@ import './index.scss'
 
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
+import { useEffect, useState } from 'react'
+import { getChannelAPI } from '@/apis/article'
 
 const { Option } = Select
 
 const Publish = () => {
+  const [channelList, setChannelList] = useState([])
+
+  useEffect(() => {
+    const getChannelList = async () => {
+      const res = await getChannelAPI()
+      setChannelList(res.data.channels)
+    }
+    getChannelList()
+  }, [])
+
   return (
     <div className="publish">
       <Card
@@ -49,7 +61,7 @@ const Publish = () => {
             rules={[{ required: true, message: '请选择文章频道' }]}
           >
             <Select placeholder="请选择文章频道" style={{ width: 400 }}>
-              <Option value={0}>推荐</Option>
+              {channelList.map(item => <Option key={item.id} value={item.id}>{item.name}</Option>)}
             </Select>
           </Form.Item>
           <Form.Item
