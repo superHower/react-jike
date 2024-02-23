edd66e82c91cc56550339f3313e0d86f1db3dd49
super_hower <yhhwwgg@gmail.com>
2024-02-23 16:03:55 +0800

    3-5： feat 实现Echarts组件封装

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,39 +1,13 @@
-import * as echarts from 'echarts';
-import { useEffect } from 'react';
+import BarChart from "./components/BarChart"
 
 const Home = () => {
-  useEffect(() => {
-    // 获取图表的Dom节点
-    const chartDom = document.getElementById('main');
-    // 初始化图表
-    const myChart = echarts.init(chartDom);
-    // 配置图表
-    const option = {
-      xAxis: {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-      },
-      yAxis: {
-        type: 'value'
-      },
-      series: [
-        {
-          data: [120, 200, 150, 80, 70, 110, 130],
-          type: 'bar'
-        }
-      ]
-    };
-    // 设置图表配置项
-    option && myChart.setOption(option);
-  }, []);
+
   return (
     <div>
-      <h1>Home</h1>
-      <p>Welcome to the Home page</p>
-      <div id='main' style={{width: '500px', height: '400px'}}>
-
-      </div>
+      <BarChart title={'三大框架满意度'} />
+      <BarChart title={'三大框架使用度'} />
     </div>
-  );
+  )
 }
-export default Home;
+
+export default Home
